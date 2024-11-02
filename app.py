4f87ce10c4ddc3bbb6df76618dd05fb4c703e051
subin@ontash.net <subin@ontash.net>
2024-11-02 10:07:11 +0530

    second-commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/chat', methods=['GETO', 'POST'])
+@app.route('/chat', methods=['GET', 'POST'])
 def chat():
     message = request.form['msg']
     return response(message)
